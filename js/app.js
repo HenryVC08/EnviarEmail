14f78b45c1938e361fb5d049fdecd2d2384bc168
HenryVC08 <henryvaldezcusi.08@gmail.com>
2025-03-10 20:52:48 -0500

    Obteniendo valores del formulario

---
+++ js/app.js
@@ -0,0 +1,8 @@
+document.addEventListener('DOMContentLoaded', function(){
+    //Seleccionando los elementos de la interfaz
+    const email = document.querySelector('#email')
+    const inputAsunto  = document.querySelector('#asunto')
+    const inputMensaje = document.querySelector('#mensaje')
+
+    console.log(inputMensaje)
+})
