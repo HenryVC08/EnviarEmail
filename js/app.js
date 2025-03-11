03a6a5550a2bb6af16bf7f03e864cd2e8626e9a3
HenryVC08 <henryvaldezcusi.08@gmail.com>
2025-03-10 21:27:27 -0500

    Mostrando error en HTML

--- js/app.js
+++ js/app.js
@@ -1,8 +1,34 @@
 document.addEventListener('DOMContentLoaded', function(){
     //Seleccionando los elementos de la interfaz
-    const email = document.querySelector('#email')
+    const inputEmail = document.querySelector('#email')
     const inputAsunto  = document.querySelector('#asunto')
     const inputMensaje = document.querySelector('#mensaje')
+    const formulario = document.querySelector('#formulario')
 
-    console.log(inputMensaje)
+    //Asignando elementos 
+    //blur es un evento que se dispara cuando sales del input
+    inputEmail.addEventListener('blur', validar)
+
+    inputAsunto.addEventListener('blur', validar)
+
+    inputMensaje.addEventListener('blur', validar)
+
+
+    function validar(e){
+        if(e.target.value.trim() === ''){
+            mostrarAlerta()
+        }else{
+            console.log('No esta vacio')
+        }
+    }
+
+    function mostrarAlerta(){
+        //Generar alerta en HTML
+        const error = document.createElement('P')
+        error.textContent = 'Hubo un error'
+        error.classList.add('bg-red-600','text-white','p-2','text-center')
+
+        //inyectando el error al formulario 
+        formulario.appendChild(error)
+    }
 })
