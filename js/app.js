3a264f0901a8b672f49330dce98ba65347619520
HenryVC08 <henryvaldezcusi.08@gmail.com>
2025-03-13 19:59:59 -0500

    Validando Email

--- js/app.js
+++ js/app.js
@@ -1,4 +1,8 @@
 document.addEventListener('DOMContentLoaded', function(){
+
+    
+
+
     //Seleccionando los elementos de la interfaz
     const inputEmail = document.querySelector('#email')
     const inputAsunto  = document.querySelector('#asunto')
@@ -16,16 +20,26 @@ document.addEventListener('DOMContentLoaded', function(){
 
     function validar(e){
 
-        console.log(e.target.parentElement)
-        console.log(e.target.id)
+        // console.log(e.target.parentElement)
+        // console.log(e.target.id)
         if(e.target.value.trim() === ''){
             mostrarAlerta(`El campo ${e.target.id} es obligatorio`, e.target.parentElement)
-        }else{
-            console.log('No esta vacio')
+            return
         }
+
+        if(e.target.id === 'email' && !validarEmail(e.target.value)) {
+            mostrarAlerta('El email no es valido', e.target.parentElement)
+            return
+        }
+
+        limpiarAlerta(e.target.parentElement)
+
     }
 
     function mostrarAlerta(mensaje, referencia){
+        //Comprueba si ya existe una alerta 
+        limpiarAlerta(referencia)
+
         //Generar alerta en HTML
         const error = document.createElement('P')
         error.textContent = mensaje
@@ -33,5 +47,22 @@ document.addEventListener('DOMContentLoaded', function(){
 
         //inyectando el error al formulario 
         referencia.appendChild(error)
+    }
+
+
+    function limpiarAlerta(referencia){
+        
+        const alerta = referencia.querySelector('.bg-red-600')
+        console.log(alerta)
+
+        if(alerta){
+            alerta.remove()
+        }
+    }
+
+    function validarEmail(email){
+        const validEmail =  /^\w+([.-_+]?\w+)*@\w+([.-]?\w+)*(\.\w{2,10})+$/;
+        const resultado = validEmail.test(email)
+        return resultado
     }
 })
